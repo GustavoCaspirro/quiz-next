b1f985eafaee1575ee9d2f51793c5c8a3b1fb52e
gustavocaspirro-mb <gustavo.caspirro@meubank.com>
2021-02-08 09:44:50 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,54 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const BackgroundImage = styled.div `
+//   background-image: url(${db.bg});
+//   background-position: center;
+//   background-repeat: no-repeat;
+//   background-size: cover; 
+//   color: ${({ theme }) => theme.colors.secondary};
+//   flex: 1;
+// `;
+
+export const QuizContainer = styled.div `
+  margin: auto 10%;
+  max-width: 350px;
+  padding-top: 45px;
+  width: 100%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={ db.bg }>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Título</h1>
+          </Widget.Header>
+          <Widget.Content>
+
+            <p>Descriçāo para o widget!</p>
+          </Widget.Content>
+        </Widget>
+        
+        <Widget>
+          <Widget.Content>
+            <h2>Quiz da Galera :)</h2>
+            <p>Descriçāo para o Quiz!</p>
+          </Widget.Content>
+        </Widget>
+
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://www.meubank.com"/>
+    </QuizBackground>
+  )
 }
---
+++ src/components/doc.js
@@ -0,0 +1,9 @@
+// Proximos Passos
+
+
+// - Corrigir background
+// - Subir para Repositório
+// - Deploy na vercel
+// - Colocar omg-image para compartilhamento
+// - https://metatags.io/
+// - https://nextjs.org/docs/api-reference/next/head
